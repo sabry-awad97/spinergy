44fbff2412b23a42182c3875adff5e3c1118b6aa
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-20 13:51:48 +0300

    🎉 feat(spinner): Add update functionality and tests
    The `SpinnerMessage` enum has been updated to include an `Update` variant, which accepts a `SpinnerResult<UpdateMessage>` to send update messages to the spinner. Additionally, a new `UpdateMessage` enum has been introduced to represent different types of update messages, starting with a `Text` variant.
    
    In the `SpinnerState` struct, a new `update` method has been added to allow updating the spinner's state with an `UpdateMessage`. The method sends the update message through a channel and returns a `SpinnerResult` indicating success or failure.
    
    Unit tests have been implemented for the `update` method to verify its functionality. The test ensures that the update message is correctly sent through the channel and received as a `SpinnerMessage::Update` variant with an `Ok` result. It also checks if the received update message matches the `UpdateMessage::Text` variant.
    
    Changes made:
    - Added `Update` variant to the `SpinnerMessage` enum.
    - Added `UpdateMessage` enum to represent different types of update messages, starting with `Text` variant.
    - Updated import statements in `spinner/state.rs` to accommodate the new types.
    - Added an `update` method to `SpinnerState` to handle update messages.
    - Implemented unit tests for the `update` method.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/spinner/message.rs
+++ src/spinner/message.rs
@@ -1,4 +1,12 @@
+use crate::SpinnerResult;
+
 #[derive(Debug, Clone)]
 pub enum SpinnerMessage {
     Stop,
+    Update(SpinnerResult<UpdateMessage>),
+}
+
+#[derive(Debug, Clone)]
+pub enum UpdateMessage {
+    Text(String),
 }
--- src/spinner/state.rs
+++ src/spinner/state.rs
@@ -1,5 +1,5 @@
-use super::channel::Channel;
-use crate::spinner::message::SpinnerMessage;
+use super::{channel::Channel, message::UpdateMessage};
+use crate::{spinner::message::SpinnerMessage, SpinnerError, SpinnerResult};
 
 #[derive(Clone)]
 pub struct SpinnerState {
@@ -10,5 +10,29 @@ impl SpinnerState {
     pub fn new() -> Self {
         let channel = Channel::new();
         Self { channel }
+    }
+
+    pub fn update(&mut self, message: UpdateMessage) -> SpinnerResult<()> {
+        self.channel
+            .try_send(SpinnerMessage::Update(Ok(message)))
+            .map_err(|_| SpinnerError::new("Failed to send message through channel"))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::spinner::message::SpinnerMessage;
+
+    #[test]
+    fn test_update() {
+        let mut state = SpinnerState::new();
+        let message = UpdateMessage::Text("test".to_owned());
+        state.update(message.clone()).unwrap();
+        let received_message = state.channel.try_receive().unwrap();
+        assert!(matches!(received_message, SpinnerMessage::Update(Ok(_))));
+        if let SpinnerMessage::Update(Ok(received_update)) = received_message {
+            assert!(matches!(received_update, UpdateMessage::Text(_)))
+        }
     }
 }
