9210aa94c99dfff79c315731f595870add7b0559
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-26 00:06:06 +0300

    ✅ test(spinner/alignment.rs): Add unit tests for Alignment module Added unit tests for the `Alignment` module to ensure correct behavior and functionality. - `test_get_horizontal_padding_left`: Test the `get_horizontal_padding` method for the `Alignment::Left` case. - `test_get_horizontal_padding_right`: Test the `get_horizontal_padding` method for the `Alignment::Right` case. - `test_get_horizontal_padding_center`: Test the `get_horizontal_padding` method for the `Alignment::Center` case. - `test_from_invalid`: Test the `from` method when an invalid alignment string is provided. It should default to `Alignment::Left`. - `test_from_lowercase`: Test the `from` method when a lowercase alignment string is provided. It should correctly convert to the corresponding `Alignment` enum variant. - `test_from_uppercase`: Test the `from` method when an uppercase alignment string is provided. It should correctly convert to the corresponding `Alignment` enum variant. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/spinner/alignment.rs
+++ src/spinner/alignment.rs
@@ -42,3 +42,65 @@ impl std::fmt::Display for Alignment {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_horizontal_padding_left() {
+        let alignment = Alignment::Left;
+        let terminal_width = 10;
+        let text_width = 5;
+        let expected_padding = "";
+
+        let padding = alignment.get_horizontal_padding(terminal_width, text_width);
+
+        assert_eq!(padding, expected_padding);
+    }
+
+    #[test]
+    fn test_get_horizontal_padding_right() {
+        let alignment = Alignment::Right;
+        let terminal_width = 10;
+        let text_width = 5;
+        let expected_padding = "     ";
+
+        let padding = alignment.get_horizontal_padding(terminal_width, text_width);
+
+        assert_eq!(padding, expected_padding);
+    }
+
+    #[test]
+    fn test_get_horizontal_padding_center() {
+        let alignment = Alignment::Center;
+        let terminal_width = 10;
+        let text_width = 5;
+        let expected_padding = "  ";
+
+        let padding = alignment.get_horizontal_padding(terminal_width, text_width);
+
+        assert_eq!(padding, expected_padding);
+    }
+
+    #[test]
+    fn test_from_invalid() {
+        let alignment_str = "invalid";
+        let alignment = Alignment::from(alignment_str);
+        assert!(matches!(alignment, Alignment::Left));
+    }
+
+    #[test]
+    fn test_from_lowercase() {
+        let alignment_str = "center";
+        let alignment = Alignment::from(alignment_str);
+        assert!(matches!(alignment, Alignment::Center));
+    }
+
+    #[test]
+    fn test_from_uppercase() {
+        let alignment_str = "CENTER";
+        let alignment = Alignment::from(alignment_str);
+        assert!(matches!(alignment, Alignment::Center));
+    }
+}
