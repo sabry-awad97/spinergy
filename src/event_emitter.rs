aac0b5b8ddeeababddb2e92af02a266bf1232bc1
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-26 18:38:24 +0300

    ✅ test(event_emitter.rs): Add unit test for EventEmitter functionality A new unit test was added to ensure the proper functionality of the `EventEmitter` struct.
    The test verifies the following behavior:
    1. Creates a new instance of `EventEmitter`.
    2. Defines a counter to track the number of times an event is emitted.
    3. Defines a listener function that increments the counter and prints the event arguments.
    4. Registers the listener for the "my_event" event.
    5. Emits the "my_event" event with some arguments.
    6. Verifies that the listener was invoked and the counter was incremented.
    
    This test helps ensure the correctness of the event emission and listener handling in the `EventEmitter` implementation.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/event_emitter.rs
+++ src/event_emitter.rs
@@ -31,3 +31,48 @@ impl EventEmitter {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::{Arc, Mutex};
+
+    use super::*;
+
+    #[test]
+    fn test_event_emitter() {
+        // Create a new EventEmitter
+        let mut emitter = EventEmitter::new();
+
+        // Define a counter for tracking the number of times the event is emitted
+        let counter = Arc::new(Mutex::new(0));
+        let counter_clone = counter.clone();
+
+        // Define a listener function
+        let listener = move |args: &[Box<dyn std::any::Any>]| {
+            *counter_clone.lock().unwrap() += 1;
+            println!("Event emitted with arguments:");
+            for arg in args {
+                if let Some(value) = arg.downcast_ref::<i32>() {
+                    println!("i32: {}", value);
+                } else if let Some(value) = arg.downcast_ref::<&str>() {
+                    println!("&str: {}", value);
+                } else if let Some(value) = arg.downcast_ref::<f64>() {
+                    println!("f64: {}", value);
+                } else {
+                    println!("Unknown type");
+                }
+            }
+        };
+
+        // Register the listener for the "my_event" event
+        emitter.on("my_event", listener);
+
+        // Emit the "my_event" event with some arguments
+        let args: Vec<Box<dyn std::any::Any>> =
+            vec![Box::new(42), Box::new("Hello"), Box::new(3.14)];
+        emitter.emit("my_event", &args);
+
+        // Verify that the listener was invoked and the counter was incremented
+        assert_eq!(*counter.lock().unwrap(), 1);
+    }
+}
